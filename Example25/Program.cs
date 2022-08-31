8369812a494bbc10e7b981c43fef300406eb50e2
Dmitry <>
2022-08-31 20:24:24 +0700

    add example 25

--- Example25/Program.cs
+++ Example25/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+int a = 2;
+int b = 4;
+
+System.Console.WriteLine($"{a}, {b} -> {pow(a, b)}");
+
+int pow(int a, int b){
+    int result = a;
+
+    for (int i = 1; i < b; i++)
+    {
+        result *= a;
+    }
+
+    return result;
+}
