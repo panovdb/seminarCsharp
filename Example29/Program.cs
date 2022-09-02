d44cb7d8540c49a79421b9c21355cf49213c991d
Dmitry <>
2022-09-03 00:27:22 +0700

    fix example 29, add fun

--- Example29/Program.cs
+++ Example29/Program.cs
@@ -5,30 +5,45 @@
 6, 1, 33 -> [6, 1, 33]
 */
 
-string textNumbers = String.Empty;
+string textNumbers = TextNumbersFromConsole();
 
-System.Console.WriteLine("Введите цифру для индекса 0");
-textNumbers = Console.ReadLine();
-
-for (int i = 1; true; i++)
-{
-    System.Console.WriteLine($"Введите цифру для индекса {i}, чтобы закончить ввод оставте строчку пустой");
-    string number = Console.ReadLine();
-    if (number == "") break;
-    textNumbers = textNumbers + ", " + number;
-}
 System.Console.Write(textNumbers);
+System.Console.Write(" -> ");
+PrintArray(CreateArrayFromText(textNumbers));
 
-string[] text = textNumbers.Split(", ");
 
-System.Console.Write(" -> [");
-
-foreach (var item in text)
+string[] CreateArrayFromText(string textNumbers)
 {
-    System.Console.Write(item + ", ");
+    string[] text = textNumbers.Split(", ");
+    return text;
 }
-int origRow = Console.CursorTop;
-int origCol = Console.CursorLeft;
-Console.SetCursorPosition(origCol - 2, origRow);
-System.Console.WriteLine("]");
+
+string TextNumbersFromConsole()
+{
+    string textNumbers = String.Empty;
+    System.Console.WriteLine("Введите цифру для индекса 0");
+    textNumbers = Console.ReadLine();
+
+    for (int i = 1; true; i++)
+    {
+        System.Console.WriteLine($"Введите цифру для индекса {i}, чтобы закончить ввод оставте строчку пустой");
+        string number = Console.ReadLine();
+        if (number == "") break;
+        textNumbers = textNumbers + ", " + number;
+    }
+    return textNumbers;
+}
+
+void PrintArray(string[] array)
+{
+    System.Console.Write("[");
+    foreach (var item in array)
+    {
+        System.Console.Write(item + ", ");
+    }
+    int origRow = Console.CursorTop;
+    int origCol = Console.CursorLeft;
+    Console.SetCursorPosition(origCol - 2, origRow);
+    System.Console.WriteLine("]");
+}
 
