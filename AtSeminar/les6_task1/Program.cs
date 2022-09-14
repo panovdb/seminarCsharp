cad308f1098cef1f02f183f24fc80bc716bd5ddc
Dmitry <>
2022-09-14 20:36:35 +0700

    add example 41, 43

---
+++ AtSeminar/les6_task1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Павел Гуляев: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+делать через строку -> int в стринг*/
+
+
+int n = -10;
+System.Console.WriteLine(ConvertToBin(n));
+
+string ConvertToBin(int n)
+{
+    string res = string.Empty;
+    while(n != 0)
+    {
+        res = n % 2 + res;
+        n = n / 2;
+    }
+
+    return res;
+}
---
+++ Example41/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+int[] array = CreateIntArrayFromConsole();
+int count = CountPositiveNumbers(array);
+PrintArray(array);
+System.Console.WriteLine(" -> " + count);
+
+
+
+int[] CreateIntArrayFromConsole()
+{   
+    System.Console.WriteLine("Введите количество чисел которые нужно ввести в программу");
+    int m = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[m];
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"Введите цифру для индекса {i}");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+int CountPositiveNumbers(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) result++;
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    foreach (var item in array)
+    {
+        System.Console.Write(item + ", ");
+    }
+    int origRow = Console.CursorTop;
+    int origCol = Console.CursorLeft;
+    Console.SetCursorPosition(origCol - 2, origRow);
+    System.Console.Write("]");
+}
