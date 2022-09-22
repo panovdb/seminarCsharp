fc102563a24f7d6d8ceab776aaa3b1243e2984e9
Dmitry <>
2022-09-22 20:27:28 +0700

    add example seminar

---
+++ AtSeminar/les9Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+*/
+
+string result = string.Empty;
+PrintNaturalInt(100);
+
+void PrintNaturalInt(int n)
+{
+    if(n > 1) PrintNaturalInt(n - 1);
+    System.Console.Write(n + " ");
+
+    /*System.Console.Write(n + " ");
+    if(n > 1) PrintNaturalInt(n - 1);*/
+    
+}
---
+++ AtSeminar/les9Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Павел Гуляев: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+*/
+int sum = 0;
+System.Console.WriteLine(SumOfDigits(23422));
+
+int SumOfDigits(int n) 
+{
+    if(n > 0) return SumOfDigits(n / 10) + n % 10;
+    else return 0;
+}
